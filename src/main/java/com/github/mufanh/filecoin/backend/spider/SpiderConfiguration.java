67aa017b065f80d2f59f9437de126275c48e0ca6
xqhuang <xqhuang@chinaums.com>
2021-01-30 16:26:18 +0800

    bugfix

--- src/main/java/com/github/mufanh/filecoin/backend/spider/SpiderConfiguration.java
+++ src/main/java/com/github/mufanh/filecoin/backend/spider/SpiderConfiguration.java
@@ -3,7 +3,6 @@ package com.github.mufanh.filecoin.backend.spider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import us.codecraft.webmagic.Spider;
 
 import java.util.List;
 
@@ -22,19 +21,5 @@ public class SpiderConfiguration {
     @Bean
     public SpiderPipeline filecoinPipeline() {
         return new SpiderPipeline();
-    }
-
-    @Bean
-    public Spider filecoinSpider(SpiderProperties properties,
-                                 SpiderPageProcessor spiderPageProcessor,
-                                 SpiderPipeline spiderPipeline,
-                                 List<PageHandler> pageHandlers) {
-        return Spider.create(spiderPageProcessor)
-                .addPipeline(spiderPipeline)
-                .addUrl(pageHandlers.stream()
-                        .map(PageHandler::url)
-                        .toArray(String[]::new))
-                .thread(properties.getThreadNum())
-                .setExitWhenComplete(true);
     }
 }
--- src/main/java/com/github/mufanh/filecoin/backend/spider/SpiderScheduled.java
+++ src/main/java/com/github/mufanh/filecoin/backend/spider/SpiderScheduled.java
@@ -1,12 +1,15 @@
 package com.github.mufanh.filecoin.backend.spider;
 
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.Spider;
+
+import java.util.List;
 
 /**
  * 爬虫调度
@@ -18,15 +21,36 @@ import us.codecraft.webmagic.Spider;
 public class SpiderScheduled implements ApplicationRunner {
 
     @Autowired
-    private Spider filecoinSpider;
+    private SpiderPageProcessor spiderPageProcessor;
+
+    @Autowired
+    private SpiderPipeline spiderPipeline;
+
+    @Autowired
+    private List<PageHandler> pageHandlers;
+
+    @Autowired
+    private SpiderProperties spiderProperties;
 
     @Scheduled(cron = "${spider.cron}")
     public void filecoinSpiderScheduled() {
-        filecoinSpider.start();
+        runFilecoinSpider();
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        filecoinSpider.start();
+        runFilecoinSpider();
+    }
+
+    public void runFilecoinSpider() {
+        Spider spider = Spider.create(spiderPageProcessor)
+                .addPipeline(spiderPipeline)
+                .addUrl(pageHandlers.stream()
+                        .map(PageHandler::url)
+                        .toArray(String[]::new))
+                .thread(spiderProperties.getThreadNum())
+                .setExitWhenComplete(true);
+        spider.start();
+        spider.stop();
     }
 }
